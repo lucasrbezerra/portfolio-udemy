5c20107916f5621c6955f32a6db21ca9387cbcb9
lucasrbezerra <lucas_ribeiro35@hotmail.com>
2021-02-07 11:46:06 -0300

    first commit

---
+++ scripts/scrollNavbar.js
@@ -0,0 +1,11 @@
+function scrollNavbar(){
+    var navbar = document.getElementById("navbar");
+    var sticky = navbar.offsetTop;
+
+    if(window.pageYOffset >= sticky){
+        navbar.classList.add("sticky");
+    }else{
+        navbar.classList.remove("sticky");
+    }
+    
+}
